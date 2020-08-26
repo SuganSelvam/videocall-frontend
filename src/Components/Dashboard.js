ce0518b548f5e4a0214969bc2b8fb0e1eda20394
Sugan Selvam <sugan170@gmail.com>
2020-08-26 12:27:12 +0530

    working inital stream

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,23 +1,9 @@
 // Importing React and Client Socket Headers
 import React, { useState } from "react";
-import io from "socket.io-client";
-import Peer from "peerjs";
 
 //Change Endpoint to local Host if You are runnign along with backend
 // const ENDPOINT = "http://localhost:4040/";
 const ENDPOINT = "https://task1-backend.herokuapp.com/";
-
-//Creating socket to connect to SERVER
-var socket = io.connect(ENDPOINT);
-
-var VideoBox = document.getElementById("video-box");
-
-const myPeer = new Peer(undefined, { host: "/", port: "4040" });
-
-const miniVideo = document.createElement("video");
-miniVideo.muted = true;
-
-const peers = {};
 
 navigator.getUserMedia = (
   navigator.getUserMedia ||
@@ -33,40 +19,34 @@ function clicked() {
       audio: true,
     })
     .then((stream) => {
+      var miniVideo = document.createElement("video")
+      miniVideo.setAttribute("id","minivideo")
+      miniVideo.muted=true
+      miniVideo.onloadedmetadata={onload}
       startStream(miniVideo, stream);
-      console.log("Stream");
+      console.log("Stream",stream,miniVideo);
     });
 }
 
 function startStream(miniVideo, stream) {
   miniVideo.srcObject = stream;
-  miniVideo.addEventListener("loadmetadata", () => {
-    console.log("Meta data");
-    var playPromise = miniVideo.play();
-    if (playPromise !== undefined) {
-      playPromise
-        .then((_) => {
-          // Automatic playback started!
-          // Show playing UI.
-          console.log("audio played auto");
-        })
-        .catch((error) => {
-          // Auto-play was prevented
-          // Show paused UI.
-          console.log("playback prevented");
-        });
-    }
-  });
-  VideoBox.appendChild(miniVideo);
+  onload(miniVideo);
+  document.getElementById("videobox").append(miniVideo);
+}
+
+ function onload(miniVideo){
+  console.log("Meta data");
+  miniVideo.play();
 }
 
 function Dashboard(props) {
   return (
     <div id="main">
       <button type="button" onClick={clicked}>
-        CLick here
+        Start Video Stream
       </button>
-      <div id="video-box"></div>
+      <div id="videobox">
+      </div>
     </div>
   );
 }
